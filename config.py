30b47130d8c55fe86e865f55e9181f2b421b3d83
nazgul180303 <nazgul180303@gmail.com>
2023-11-06 17:25:15 +0600

    'jhfjfckj'

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from decouple import config
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+PROXY_URL = 'http:proxy.server:3128'
 storage = MemoryStorage()
 TOKEN = config("TOKEN")
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN, proxy=PROXY_URL)
 DESTINATION = '/Users/User/PycharmProjects/FirstBot'
 dp = Dispatcher(bot=bot, storage=storage)
 GROUP_ID = [-4014883320]
